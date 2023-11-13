558502b3efe93f97cad0a6f1c99de891b9b0de20
Igor <80968651+webdevbiv@users.noreply.github.com>
2023-11-13 17:32:02 +0200

    ADD | tuple enum

--- module-02/project-02/app.js
+++ module-02/project-02/app.js
@@ -1,5 +1,25 @@
+var User;
+(function (User) {
+    User[User["ADMIN"] = 0] = "ADMIN";
+    User[User["READ_ONLY"] = 1] = "READ_ONLY";
+    User[User["AUTHOR"] = 2] = "AUTHOR";
+})(User || (User = {}));
 var person = {
     name: "Max",
     age: 30,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"],
+    user: User.ADMIN, //enum
 };
-console.log(person.name);
+person.role.push("admin");
+// person.role[1] = 10;
+var favoriteActivities;
+favoriteActivities = ["Sports", 1];
+console.log(person.hobbies);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.user === User.ADMIN) {
+    console.log("is admin");
+}
--- module-02/project-02/app.ts
+++ module-02/project-02/app.ts
@@ -1,6 +1,35 @@
-const person: { name: string; age: number } = {
+enum User {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple
+  user: User; //enum
+} = {
   name: "Max",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"], //tuple
+  user: User.ADMIN, //enum
 };
 
-console.log(person.name);
+person.role.push("admin");
+// person.role[1] = 10;
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", 1];
+
+console.log(person.hobbies);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.user === User.ADMIN) {
+  console.log("user rights admin");
+}
