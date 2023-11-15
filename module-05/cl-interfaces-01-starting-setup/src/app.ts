4869edb33cf3b718de8a27a955487eeedc05f3a1
Igor <80968651+webdevbiv@users.noreply.github.com>
2023-11-15 18:03:42 +0200

    ADD | interfaces

--- module-05/cl-interfaces-01-starting-setup/src/app.ts
+++ module-05/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,120 +1,68 @@
-//SECTION - CLASSES
-class Department {
-  // name: string;
-  private employees: string[] = []; // private makes it only accessible within the class
-  constructor(public name: string, private readonly id: number) {}
+//SECTION - INTERFACES
+//NOTE - function
 
-  describe(this: Department) {
-    console.log("Department: " + this.name + " ID: " + this.id);
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+interface Named {
+  readonly name: string;
+  outputName?: string; //NOTE - optional
+}
+
+interface Person extends Named {
+  age: number;
+  outputName?: "test";
+
+  greet(phrase: string): void;
+}
+
+let user1: Person;
+
+user1 = {
+  name: "Max",
+  age: 30,
+  outputName: "test",
+
+  greet(phrase: string) {
+    console.log("user1: " + phrase + " " + this.name + " " + this.outputName);
+  },
+};
+
+user1.greet("Hi there - I am");
+
+//SECTION - CLASSES & INTERFACES
+
+interface Greetable {
+  name: string;
+  greet(phrase: string): void;
+}
+
+let user2: Greetable;
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(name: string) {
+    this.name = name;
   }
 
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
   }
 }
 
-const accounting = new Department("Accounting", 1);
+let user3: Greetable;
 
-accounting.addEmployee("Max");
-accounting.addEmployee("Jim");
+user3 = new Person("Max");
 
-// accounting.employees[2] = "Anna";
+user3.greet("Hi there - I am");
 
-accounting.describe();
-accounting.printEmployeeInformation();
-
-// const accountingCopy = { name: "Dummy", describe: accounting.describe };
-
-// accountingCopy.describe();
-
-//SECTION - INHERITANCE, PROTECTED & PRIVATE, STATIC
-class Department2 {
-  protected employees: string[] = [];
-  static fiscalYear = 2020;
-
-  constructor(private readonly id: string, public name: string) {}
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
-
-  describe(this: Department2) {
-    console.log("Department: " + this.name + " ID: " + this.id);
-  }
-}
-
-const employee1 = Department2.createEmployee("Max");
-console.log(employee1, "employee1", Department2.fiscalYear);
-
-class ITDepartment extends Department2 {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
-}
-
-const it = new ITDepartment("d1", ["Max"]);
-
-console.log(it);
-
-class AccountingDepartment extends Department2 {
-  private lastReport: string;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found");
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value");
-    }
-    this.addReport(value);
-  }
-  constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
-
-  addEmployee(employee: string): void {
-    if (employee === "Max") {
-      return;
-    }
-    this.employees.push(employee);
-  }
-
-  addReport(report: string) {
-    this.reports.push(report);
-    this.lastReport = report;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const accounting2 = new AccountingDepartment("d2", []);
-
-console.log(accounting2);
-
-console.log(accounting2);
-
-accounting2.addReport("Weekly Report");
-accounting2.mostRecentReport = "Year End Report";
-console.log(accounting2.mostRecentReport);
+console.log(user3);
